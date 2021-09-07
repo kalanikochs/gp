576e198d76d733126d4d36cad8cf30a42a8f1f2c
Marcos Gabriel Robles Gonzalez <mrobles@solomediagroup.co>
2021-09-07 07:59:11 -0500

    prepared-to-prod

--- api/app/config/config-prod.php
+++ api/app/config/config-prod.php
@@ -3,11 +3,11 @@
     //acceso a la base de datos
     define('APP_PATH',dirname(dirname(__FILE__)));
     define('DB_HOST','localhost');
-    define('DB_USER','ijxclrfi_gpuser');
-    define('DB_PASSWORD','wRxB*~5}Ih%7');
-    define('DB_NAME','ijxclrfi_gastropack');
-    define('URL_PATH','https://entiven.com/api/');
-    define('STORAGE_PATH','https://entiven.com/api/');
+    define('DB_USER','u644490110_gastropack_adm');
+    define('DB_PASSWORD','Gastropack_pwd1');
+    define('DB_NAME','u644490110_gastropack_pro');
+    define('URL_PATH','https://api.sintonizateconlavida.com/');
+    define('STORAGE_PATH','https://api.sintonizateconlavida.com/storage/');
 
 
 ?>
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { common } from './common';
 
 const local = {
-  apiUrl: 'https://entiven.com/api',
+  apiUrl: 'https://api.sintonizateconlavida.com',
   fbServerKey: 'AAAAAcePNFE:APA91bEpTTS8rfaALyhQezgI9yYXJd5EkrXznXdvSMkHNnrmtIFVF6eW4JZw8j7hD0u8nnjM5_a0ug2DHJ8pBUzY-sbHL8zavMINbQjN9hhADrU6utHKulvVec0b3NerJnMmypsXKUXY',
   fbSenderID: '7643018321'
 };
